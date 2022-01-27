42b5c0a6256658325f19eeca0b055df11d8b8ece
Fkkuforiji <fkkuforiji@gmail.com>
2022-01-27 02:25:24 -0500

    third commit, january 25th lecture

--- src/main/java/perscholas/controller/IndexController.java
+++ src/main/java/perscholas/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletRequest;
 
+
 @Controller
 //the C in MVC
 public class IndexController {
@@ -27,14 +28,26 @@ public class IndexController {
 Model is an object that stores values that need to be sent to the jsp file. Store in strings.*/
        String username = request.getParameter("username");
        //This string username is coming from the Username named "username" in the Index.jsp file
-        String firstname = request.getParameter("firstName");
+        String firstName = request.getParameter("firstName");
         //This string username is coming from the First Name named "firstName" in the Index.jsp file
-
-        System.out.println("username = " + request.getParameter("username"));
-        //This prints out the input received in the username form on the webpage
-        System.out.println("First Name = " +request.getParameter("firstName"));
-
+        String email = request.getParameter("email");
+        String birthdate = request.getParameter("birthdate");
+        System.out.println("username = " + username);
+        //This prints out the input received in the username form on the webpage.
+        //before we had sout("username = " + request.getParameter("username);
+        //String username is defined as username from index, and used in the print out statement
+        System.out.println("First Name = " + firstName);
+        System.out.println("Email = " + email);
+        System.out.println("birthdate = " + birthdate);
         ModelAndView response = new ModelAndView();
+        response.addObject("username",username);
+        response.addObject("firstName",firstName);
+        response.addObject("email", email);
+        response.addObject("birthdate", birthdate);
+        /*response is the String name for the model object variable, this can be named anything. addObject adds a specific object
+        from the object defined in index and makes it available to render in the indexSubmit page.
+        now, the username and firstname inputted into the form, are available to display
+        on indexSubmit page*/
         response.setViewName("indexSubmit");
         //this response view routes to a viewing a webpage named indexSubmit, rather than the index.
         //for this to route to indexSubmit, a jsp file must be created.
--- src/main/webapp/WEB-INF/jsp/indexSubmit.jsp
+++ src/main/webapp/WEB-INF/jsp/indexSubmit.jsp
@@ -1,8 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%--look up what ^^ means. Don't know if it was needed.
+It was in the examples jsp index file on Eric's github--%>
 <html>
 
 <body>
 
 <h1>Index Submit Page</h1>
 
+<h2>Hello ${firstName} your username is ${username} your email is ${email} and your birthdate is ${birthdate}</h2>
+<!--These are objects from the IndexController under the indexSubmit method
+that were made available to this jsp file by using addObject-->
 </body>
 </html>
